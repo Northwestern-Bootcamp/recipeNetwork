7b3e02a0212057dacc5efde876656b95bd897000
Quanisha Jackson <jquanisha0031@gmail.com>
2021-05-17 18:44:37 -0500

    Made cards hidden on load page. Added More Info

--- assets/functions.js
+++ assets/functions.js
@@ -5,8 +5,8 @@ var recipeExtraInfoContainer = document.getElementById('recipeExtraInfoContainer
 
 
 //searchResults page + extra recipe detail variables
-var recipeSearchResultContainer 
-var recipeSearchResultImage 
+var recipeSearchResultContainer
+var recipeSearchResultImage
 var recipeSearchResultHeadings
 var recipeSearchResultButton
 var cardImage = document.querySelectorAll(".card-image");
@@ -23,63 +23,65 @@ var recipeSearchInput = document.getElementById('recipe-search-input')
 function getApi(e) {
     e.preventDefault();
     // Insert the API url to get a list of your repos
-     var requestUrl = `https://api.edamam.com/search?q=${recipeSearchInput.value}&app_id=46923f1d&app_key=5e710e80302e239cd7c90b4b531dd159&from=1&to=7`;    
+    var requestUrl = `https://api.edamam.com/search?q=${recipeSearchInput.value}&app_id=46923f1d&app_key=5e710e80302e239cd7c90b4b531dd159&from=1&to=7`;
 
-     fetch(requestUrl)
-      .then(function (response) {
-        console.log(response)
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        data = data.hits
-        searchReults(data);
-        
-    });
+    fetch(requestUrl)
+        .then(function(response) {
+            console.log(response)
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            data = data.hits
+            searchReults(data);
+
+        });
 }
 
 function searchReults(data) {
-      
+    containerList.classList.remove("hidden")
+
+
     //looping over the fetch response and inserting the URL of your repos into a list
-        for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         recipeSearchResultContainer = document.createElement('div');
         recipeSearchResultImage = document.createElement('img');
         recipeSearchResultHeading = document.createElement('h3');
         recipeSearchResultButton = document.createElement('button');
-            // pull image (data[i].recipe.image)
-            recipeSearchResultImage.setAttribute('src', data[i].recipe.image)
-            recipeSearchResultHeading.textContent = data[i].recipe.label;           
-            recipeSearchResultButton.textContent = "More Info";
-            containerList.appendChild(recipeSearchResultContainer); 
-            // containerList.appendChild(recipeSearchResultImage);
-            cardImage[i].appendChild(recipeSearchResultImage);
-            cardContent[i].appendChild(recipeSearchResultHeading);
-            cardAction[i].appendChild(recipeSearchResultButton);
-            recipeDisplay(data[i].recipe.label);
-            // add CSS to make the button bigger            
-        }
-        console.log(recipeSearchResultButton);
+        // pull image (data[i].recipe.image)
+        recipeSearchResultImage.setAttribute('src', data[i].recipe.image)
+        recipeSearchResultHeading.textContent = data[i].recipe.label;
+        recipeSearchResultButton.textContent = "More Info";
+        containerList.appendChild(recipeSearchResultContainer);
+        // containerList.appendChild(recipeSearchResultImage);
+        cardImage[i].appendChild(recipeSearchResultImage);
+        cardContent[i].appendChild(recipeSearchResultHeading);
+        cardAction[i].appendChild(recipeSearchResultButton);
+        recipeDisplay(data[i].recipe.label);
+        // add CSS to make the button bigger            
     }
+    console.log(recipeSearchResultButton);
+}
 
-function recipeDisplay(data){
-  recipeSearchResultButton.addEventListener('click', function(){
-    console.log(data);
-  })
-}    
+function recipeDisplay(data) {
+    recipeSearchResultButton.addEventListener('click', function() {
+        console.log(data);
+    })
+}
 
 // function recipeExtraInfo {
 //     recipeSearchResultHeading.textContent = data[i].recipe.label;
 //     recipeExtraInfoContainer.appendChild(recipeSearchResultHeading);
 // }
-  
 
 
 
 
 
 
-  fetchButton.addEventListener('click', getApi);
-  /* fetchButton.addEventListener('click', function(e){
+
+fetchButton.addEventListener('click', getApi);
+/* fetchButton.addEventListener('click', function(e){
     e.preventDefault();
     console.log("button works")
     // log search result
@@ -94,8 +96,3 @@ function recipeDisplay(data){
 
 // container to attach API results to
 var repoList = document.querySelector('ul');
-
-
-
-
-
