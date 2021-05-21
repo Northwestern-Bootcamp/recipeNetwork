83ba13f13623b27fd6cdd5fdf0640a9faf8d131f
Kenneth Cheng <itskcheng@gmail.com>
2021-05-20 22:26:55 -0400

    Finished local storage feature and cleaned up the JavaScript file

--- assets/functions.js
+++ assets/functions.js
@@ -1,49 +1,58 @@
 //click on search bar and get data from it
-var repoList = document.getElementById('ingredients');
 var containerList = document.getElementById('search-results');
-var recipeExtraInfoContainer = document.getElementById('recipeExtraInfoContainer');
 var recipeContainer = document.querySelector('.recipe-display');
 
-
 //searchResults page + extra recipe detail variables
-var recipeSearchResultContainer;
-var recipeSearchResultImage;
-var recipeSearchResultHeadings;
-var recipeSearchResultButton;
-//var displayRecipeContainer;
 var displayRecipeImage;
 var displayRecipeHeading;
 var displayRecipeUnorderedList;
 var displayIngredients;
 var displayRecipeVideo;
-var cardImage = document.querySelectorAll(".card-image");
-var cardContent = document.querySelectorAll(".card-content");
-var cardAction = document.querySelectorAll(".card-action");
-var recipeImage = document.querySelector(".recipe-image")
-var recipeContent = document.querySelector(".recipe-content")
+var recipeSearchResultContainer;
+var recipeSearchResultImage;
+var recipeSearchResultHeadings;
+var recipeSearchResultButton;
+var cardImage = document.querySelectorAll('.card-image');
+var cardContent = document.querySelectorAll('.card-content');
+var cardAction = document.querySelectorAll('.card-action');
+var recipeImage = document.querySelector('.recipe-image');
+var recipeContent = document.querySelector('.recipe-content');
 
 // recipe-search-button - event listener for the click
 var fetchButton = document.getElementById('recipe-search-button');
 
 // recipe-search-input - to get the value
-var recipeSearchInput = document.getElementById('recipe-search-input')
+var recipeSearchInput = document.getElementById('recipe-search-input');
 
-var historyEl = document.querySelector(".search-history")
+//Local storage element to append to
+var historyEl = document.querySelector('.search-history');
 
-var historyList =[]
-var searchHistory = localStorage.getItem('search-history')
-console.log(searchHistory)
-if (searchHistory) {
-    searchHistory = (searchHistory)
-    var searchList = searchHistory.length
-    if (searchHistory.length > 3) {
-        searchList = 3
-    }
-    for (var i= 0; i < searchList; i++){
-        var listItem = document.createElement('li')
-        listItem.textContent = searchHistory[i]
-        historyEl.append(listItem)
-    }
+//local storage get item
+var gettingItem = JSON.parse(localStorage.getItem('food')) || [];
+
+//Clear the search input value on page load
+recipeSearchInput.value = '';
+//local storage function to set item
+var set = function(){
+  var food = [recipeSearchInput.value];
+
+  gettingItem.push(food);
+  console.log(gettingItem);
+  localStorage.setItem('food', JSON.stringify(gettingItem));
+}
+
+//Display local storage
+var listing = function(){
+  historyEl.innerHTML = '';
+  var searchLength = gettingItem.length;
+  if(gettingItem.length >= 3){
+    searchLength = 3;
+  }
+  for(var i = 0; i < searchLength; i++){
+      var list = document.createElement('ul');
+      list.innerHTML = `<ol>${gettingItem[gettingItem.length - i - 1]}</ol>`;
+      historyEl.append(list);
+  }
 }
 //getApi function is called when the fetchButton is clicked
 function getApi(e) {
@@ -53,21 +62,20 @@ function getApi(e) {
 
   fetch(requestUrl)
     .then(function (response) {
-      console.log(response)
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
       data = data.hits
       searchReults(data);
-
+      set();
+      listing();
+      recipeSearchInput.value = '';
     });
 }
 
+//fetch for youtube API
 function youtubeAPI(data) {
   var youtubeHeading = data.recipe.label;
-  console.log(youtubeHeading);
-
   var youtubeUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${youtubeHeading}&key=AIzaSyDIguCz_5VxSSQ_HIouV7kqUS4rNjWoi0A`;
 
   fetch(youtubeUrl)
@@ -75,58 +83,48 @@ function youtubeAPI(data) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       displayYoutubeVideos(data);
     })
 }
 
+//Displays the search results data on the page 
 function searchReults(data) {
-
-
-  containerList.classList.remove("hidden")
+  containerList.classList.remove('hidden');
 
   for (var i = 0; i < data.length; i++) {
     cardImage[i].innerHTML = '';
     cardContent[i].innerHTML = '';
     cardAction[i].innerHTML = '';
-    recipeContainer.classList.add("hidden")
-    // recipeImage.innerHTML = '';
-    // recipeContent.innerHTML = '';
+    recipeImage.innerHTML = '';
+    recipeContent.innerHTML = '';
   }
+
   //looping over the fetch response and inserting the URL of your repos into a list
   for (var i = 0; i < data.length; i++) {
     recipeSearchResultContainer = document.createElement('div');
     recipeSearchResultImage = document.createElement('img');
     recipeSearchResultHeading = document.createElement('h3');
     recipeSearchResultButton = document.createElement('button');
-    // pull image (data[i].recipe.image)
     recipeSearchResultImage.setAttribute('src', data[i].recipe.image)
     recipeSearchResultHeading.textContent = data[i].recipe.label;
-    recipeSearchResultButton.textContent = "More Info";
+    recipeSearchResultButton.textContent = 'More Info';
     containerList.appendChild(recipeSearchResultContainer);
-    // containerList.appendChild(recipeSearchResultImage);
     cardImage[i].appendChild(recipeSearchResultImage);
     cardContent[i].appendChild(recipeSearchResultHeading);
     cardAction[i].appendChild(recipeSearchResultButton);
-    recipeDisplay(data[i]);
-    // add CSS to make the button bigger            
+    recipeDisplay(data[i]);           
   }
-  // localStorage.setItem('search-history', recipeSearchInput.value)
-  // console.log(localStorage.getItem('search-history'))
-  
-    // var dropd = document.getElementById("savedrop").value;
-    
-    historyList.unshift(recipeSearchInput.value);
-    localStorage.setItem("search-history", JSON.stringify(historyList));
-    console.log(historyList)
 }
 
+//Display the recipe when the more info button is clicked on 
 function recipeDisplay(data) {
   recipeSearchResultButton.addEventListener('click', function () {
-    recipeContainer.classList.remove("hidden")
+    //shows the container by removing hidden class
+    recipeContainer.classList.remove("hidden");
+    //resets the image and content elements
     recipeImage.innerHTML = '';
-    recipeContent.innerHTML = '';    
-    console.log(data);
+    recipeContent.innerHTML = '';
+    //Creates elements for the recipe display fucntion
     displayRecipeContainer = document.createElement('div');
     displayRecipeImage = document.createElement('img');
     displayRecipeHeading = document.createElement('h3');
@@ -142,18 +140,17 @@ function recipeDisplay(data) {
       displayIngredients.textContent = data.recipe.ingredientLines[i]
       displayRecipeUnorderedList.append(displayIngredients);
     }
-
+    //Runs the youtube api function
     youtubeAPI(data);
 
+    //appending elements
     recipeImage.append(displayRecipeImage);
     recipeContent.append(displayRecipeHeading);
     recipeContent.append(displayRecipeUnorderedList);
-
-
-
   })
 }
 
+//Function to display the youtube videos
 function displayYoutubeVideos(data) {
   for (var i = 0; i < data.items.length; i++) {
     var youtubeId = data.items[i].id.videoId;
@@ -163,40 +160,10 @@ function displayYoutubeVideos(data) {
   }
 }
 
-//displayRecipeContainer;
-//displayRecipeHeading;
-//displayRecipeUnorderedList;
-//displayRecipeList;
-//displayRecipeVideo;
-
-// function recipeExtraInfo {
-//     recipeSearchResultHeading.textContent = data[i].recipe.label;
-//     recipeExtraInfoContainer.appendChild(recipeSearchResultHeading);
-// }
-
-
-
-
-
-
-
+//listens to a click that runs getApi function
 fetchButton.addEventListener('click', getApi);
-/* fetchButton.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log("button works")
-  // log search result
-  console.log(recipeSearchInput.value)
-  //add recent search
-  document.body.innerHTML = "Recent searches:"
-  //add 
 
-});
-
-*/
-
-// container to attach API results to
-var repoList = document.querySelector('ul');
-
-
+//Gets local storage items when page loads
+listing();
 
 
